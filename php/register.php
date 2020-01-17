5247e827a39050872ed9b0bc0040372706ec2e0b
michaelstrefeler <mstrefeler@me.com>
2020-01-17 09:30:11 +0100

    Modification de la mise en page

--- php/register.php
+++ php/register.php
@@ -22,35 +22,43 @@
             <h3 class="bg-gray p-4">Créer un compte</h3>
             <form action="addUser.php" method="post">
               <fieldset class="p-4">
-                <input type="text" id="firstname" name="firstName" placeholder="Prénom" class="border p-3 w-100 my-2" pattern="^[A-Z a-z-]{0,30}$" required>
-                <input type="text" id="lastname" name="lastName" placeholder="Nom" class="border p-3 w-100 my-2" pattern="^[A-Z a-z-]{0,30}$" required>
-                <input type="text" id="username" name="username" placeholder="Pseudo" class="border p-3 w-100 my-2" pattern="^[A-Z a-z_0-9-]{0,30}$" required>
-                 <select id="meansofcontact" name="meansOfContact" class="border p-3 w-100 my-2" required>
+                <input type="text" id="firstname" name="firstName" placeholder="Prénom" class="border p-3 w-100 my-2"
+                  pattern="^[A-Z a-z-]{0,30}$" required>
+                <input type="text" id="lastname" name="lastName" placeholder="Nom" class="border p-3 w-100 my-2"
+                  pattern="^[A-Z a-z-]{0,30}$" required>
+                <input type="text" id="username" name="username" placeholder="Pseudo" class="border p-3 w-100 my-2"
+                  pattern="^[A-Z a-z_0-9-]{0,30}$" required>
+                <select id="meansofcontact" name="meansOfContact" class="border p-3 w-100 my-2" required>
                   <option selected disabled>Moyen de contact</option>
                   <option value="tel">Téléphone</option>
                   <option value="email">Email</option>
                   <option value="autre">Autre</option>
-                </select> 
-                <input type="text" id="contactinfo" name="contactInfo" placeholder="Numéro de téléphone ou email ou autre" class="border p-3 w-100 my-2" required>                 
-                <input type="password" id="password" name="password" placeholder="Mot de passe" class="border p-3 w-100 my-2" pattern="^.{8,60}$" title="Minimum 8 caractères" required>
-                <input type="password" id="verifypassword" name="verifypassword" placeholder="Confirmation du mot de passe" pattern="^.{8,60}$" class="border p-3 w-100 my-2" required>
+                </select>
+                <input type="text" id="contactinfo" name="contactInfo"
+                  placeholder="Numéro de téléphone ou email ou autre" class="border p-3 w-100 my-2" required>
+                <input type="password" id="password" name="password" placeholder="Mot de passe"
+                  class="border p-3 w-100 my-2" pattern="^.{8,60}$" title="Minimum 8 caractères" required>
+                <input type="password" id="verifypassword" name="verifypassword"
+                  placeholder="Confirmation du mot de passe" pattern="^.{8,60}$" class="border p-3 w-100 my-2" required>
                 <script>
-                  var password = document.getElementById("password")
-                  , confirm_password = document.getElementById("verifypassword");
+                  var password = document.getElementById("password"),
+                    confirm_password = document.getElementById("verifypassword");
 
-                  function validatePassword(){
-                    if(password.value != confirm_password.value) {
+                  function validatePassword() {
+                    if (password.value != confirm_password.value) {
                       confirm_password.setCustomValidity("Les mots de passe ne correspondent pas!");
-                      } else {
-                        confirm_password.setCustomValidity('');
-                        }
+                    } else {
+                      confirm_password.setCustomValidity('');
+                    }
                   }
-                  
+
                   password.onchange = validatePassword;
                   confirm_password.onkeyup = validatePassword;
                 </script>
                 <br><br>
-                <button type="submit" class="d-block py-3 px-4 bg-primary text-white border-0 rounded font-weight-bold">Créer un compte</button>
+                <button type="submit"
+                  class="d-block py-3 px-4 bg-primary text-white border-0 rounded font-weight-bold">Créer un
+                  compte</button>
               </fieldset>
             </form>
           </div>
