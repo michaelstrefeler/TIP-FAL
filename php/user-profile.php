eaf8d4ea8ebc1b44cf0768d9d53ac2ba2d60c5f8
Michael Strefeler <mstrefeler@me.com>
2020-02-15 13:09:18 +0100

    Suppression de 2 formulaires

--- php/user-profile.php
+++ php/user-profile.php
@@ -66,21 +66,9 @@
 					</div>
 				</div>
 				<div class="col-md-10 offset-md-1 col-lg-8 offset-lg-0">
-					<!-- Modifier vos informations personnelles -->
+					<!-- Changer de photo -->
 					<div class="widget personal-info">
-						<h3 class="widget-header user">Modifier vos informations personnelles</h3>
-						<form method="post">
-							<div class="form-group">
-								<label for="first-name">Prénom</label>
-								<input type="text" class="form-control" id="first-name" value="<?php echo $firstName ?>">
-							</div>
-							<div class="form-group">
-								<label for="last-name">Nom</label>
-								<input type="text" class="form-control" id="last-name" value="<?php echo $lastName ?>">
-							</div>
-							<button class="btn btn-transparent">Enregistrer les modifications</button>
-						</form>
-						<br><br>
+						<h3 class="widget-header user">Changer de photo de profil</h3>
 						<form method="post" enctype="multipart/form-data" action="edit-user-photo.php">
 							<div class="form-group choose-file">
 								<i class="fa fa-user text-center"></i>
@@ -110,41 +98,6 @@
 							<button class="btn btn-transparent">Changer de mot de passe</button>
 						</form>
 					</div>
-					<!-- Changer de moyen de contact -->
-					<div class="widget change-email mb-0">
-						<h3 class="widget-header user">Changer de moyen de contact</h3>
-						<form method="post">
-							<div class="form-group">
-								<select id="meansofcontact" name="meansOfContact" class="border p-3 w-100 my-2" required>
-									<option disabled>Moyen de contact</option>
-									<?php
-										switch($meansOfContact){	
-											case "tel":
-												echo'<option value="tel" selected>Téléphone</option>
-												<option value="email">Email</option>
-												<option value="autre">Autre</option>';
-											break;
-											case "email":
-												echo'<option value="tel">Téléphone</option>';
-												echo'<option value="email" selected>Email</option>';
-												echo'<option value="autre">Autre</option>';
-											break;
-											case "autre":
-												echo'<option value="tel">Téléphone</option>
-												<option value="email">Email</option>
-												<option value="autre" selected>Autre</option>';
-											break;
-										}
-									?>
-								</select> 
-							</div>
-							<div class="form-group">
-								<label for="contactInfo">Téléphone ou E-mail</label>
-								<input type="text" class="form-control" id="contactInfo" value="<?php echo $contactInfo; ?>">
-							</div>
-							<button class="btn btn-transparent">Changer de moyen de contact</button>
-							</div>
-						</form>
 					</div>
 				</div>
 			</div>
